e35ba96aefa3dec9a41e37a5e8ff35be94301805
Péter Binder <pbinder.hu@gmail.com>
2023-11-30 16:41:22 +0100

    implementing room and single bedroom

---
+++ src/room.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class Room(ABC):
+    @property
+    @abstractmethod
+    def price(self):
+        pass
+
+    @property
+    @abstractmethod
+    def room_number(self):
+        pass
+
+
+class SingleBedRoom(Room):
+
+    def __init__(self):
+        self._price = 100
+        self._room_number = 1
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def room_number(self):
+        return self._room_number
